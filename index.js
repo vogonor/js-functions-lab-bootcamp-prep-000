88f45f6910a3a3047c0779b657aa6e3f6f6dc052
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-14 05:20:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-function happyCustomHolidayTo(oliday, person){
+function happyCustomHolidayTo(holiday, person){
   
   return `Happy ${holiday}, ${name}!`
 }
